bc00918ee33b8227e521ab5c2dbd2a48fe99cd03
mastertalent707-stack <mastertalent707@gmail.com>
2025-06-13 17:04:32 +0200

    Add basic login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 import Home from '@/pages/Home';
 import About from '@/pages/About';
+import Login from '@/pages/auth/Login';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/auth/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/auth/Login.tsx
@@ -0,0 +1,28 @@
+import minecraftBackground from '@/assets/minecraft_background.png';
+import { Input } from '@/elements/inputs';
+
+export default function Login() {
+  return (
+    <div className="flex h-screen">
+      <div className="w-1/2 h-full">
+        <img src={minecraftBackground} alt="Minecraft Background" className="w-full h-full object-cover" />
+      </div>
+      <div className="w-1/2 h-full">
+        <div className="flex flex-col items-center justify-center h-full">
+          <h1 className="text-3xl font-bold text-white">Login</h1>
+          <form className="mt-4">
+            <div className="mb-4">
+              <Input.Text id={'username'} variant={Input.Text.Variants.Loose} placeholder="Username" type="text" />
+            </div>
+            <div className="mb-4">
+              <Input.Text variant={Input.Text.Variants.Loose} placeholder="Password" type="password" />
+            </div>
+            <button type="submit" className="px-4 py-2 w-full rounded-md bg-blue-500 text-white">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
