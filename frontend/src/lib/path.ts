999d1568a2408848fff50dc5265c4facfc2fca3e
mastertalent707-stack <mastertalent707@gmail.com>
2025-06-23 15:49:06 +0200

    Fix server path regex

--- frontend/src/lib/path.ts
+++ frontend/src/lib/path.ts
@@ -1,5 +1,5 @@
-const _FILES_HOME_REGEX = /^\/server\/[a-z1-9]+\/files$/;
-const _FILES_ACTION_REGEX = /^\/server\/[a-z1-9]+\/files\/(directory|edit)(\/.*)?$/;
+const _FILES_HOME_REGEX = /^\/server\/[a-z0-9]+\/files$/;
+const _FILES_ACTION_REGEX = /^\/server\/[a-z0-9]+\/files\/(directory|edit)(\/.*)?$/;
 
 export function urlPathToFilePath(path: string): string {
   if (_FILES_HOME_REGEX.test(path)) {
