7282bb179b08c380ab12981cf29334464c3a540f
mastertalent707-stack <mastertalent707@gmail.com>
2025-06-26 16:38:20 +0200

    Fix infinite footer rendering

--- frontend/src/elements/dialog/DialogFooter.tsx
+++ frontend/src/elements/dialog/DialogFooter.tsx
@@ -1,12 +1,14 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { DialogContext } from './';
 
 export default ({ children }: { children: React.ReactNode }) => {
   const { setFooter } = useContext(DialogContext);
 
-  setFooter(
-    <div className={'px-6 py-3 bg-gray-700 flex items-center justify-end space-x-3 rounded-b'}>{children}</div>,
-  );
+  useEffect(() => {
+    setFooter(
+      <div className={'px-6 py-3 bg-gray-700 flex items-center justify-end space-x-3 rounded-b'}>{children}</div>,
+    );
+  }, [children]);
 
   return null;
 };
