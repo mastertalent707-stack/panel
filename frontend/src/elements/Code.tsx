d0fb74bfa1e5afbcb2f8996803bff6e2dd06cf92
mastertalent707-stack <mastertalent707@gmail.com>
2025-06-29 12:13:18 +0200

    Improve <Code> styling

--- frontend/src/elements/Code.tsx
+++ frontend/src/elements/Code.tsx
@@ -1,3 +1,3 @@
 export default ({ children }: { children: React.ReactNode }) => {
-  return <code className="font-mono bg-gray-800 rounded py-1 px-2">{children}</code>;
+  return <code className="font-mono bg-gray-800 rounded py-1 px-2 inline-block">{children}</code>;
 };
