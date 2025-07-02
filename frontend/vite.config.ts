ec9ad5f67c9873988a9abdbf7abec640e94af289
mastertalent707-stack <mastertalent707@gmail.com>
2025-07-02 17:11:47 +0200

    Configure vite output dir

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,4 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
+  build: {
+    outDir: './dist',
+    emptyOutDir: true,
+  },
 });
