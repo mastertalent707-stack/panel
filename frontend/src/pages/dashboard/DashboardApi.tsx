c183fdf2fe60807e04aeb7f245537b0a362d1ede
mastertalent707-stack <mastertalent707@gmail.com>
2025-07-06 21:06:44 +0200

    Add api token creation & display

--- frontend/src/pages/dashboard/DashboardApi.tsx
+++ frontend/src/pages/dashboard/DashboardApi.tsx
@@ -1,9 +1,91 @@
+import getApiKeys from '@/api/me/api/getApiKeys';
+import Code from '@/elements/Code';
 import Container from '@/elements/Container';
+import Spinner from '@/elements/Spinner';
+import Table, {
+  ContentWrapper,
+  TableHead,
+  TableHeader,
+  TableBody,
+  NoItems,
+  TableRow,
+  Pagination,
+} from '@/elements/table/Table';
+import Tooltip from '@/elements/Tooltip';
+import { formatDateTime, formatTimestamp } from '@/lib/time';
+import { useEffect, useState } from 'react';
+import ApiKeyCreateButton from './actions/ApiKeyCreateButton';
+import { useUserStore } from '@/stores/user';
 
 export default () => {
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const { keys, setKeys } = useUserStore(state => state.keys);
+
+  useEffect(() => {
+    getApiKeys(page).then(data => {
+      setKeys(data);
+      setLoading(false);
+    });
+  }, [page]);
+
   return (
     <Container>
-      <p>Dashboard API</p>
+      <div className="justify-between flex items-center mb-2">
+        <h1 className="text-4xl font-bold text-white">API Keys</h1>
+        <ApiKeyCreateButton />
+      </div>
+      {loading ? (
+        <Spinner.Centered />
+      ) : (
+        <Table>
+          <ContentWrapper>
+            <Pagination data={keys} onPageSelect={setPage}>
+              <div className="overflow-x-auto">
+                <table className="w-full table-auto">
+                  <TableHead>
+                    <TableHeader name={'Name'} />
+                    <TableHeader name={'Key'} />
+                    <TableHeader name={'Permissions'} />
+                    <TableHeader name={'Last Used'} />
+                    <TableHeader name={'Created'} />
+                  </TableHead>
+
+                  <TableBody>
+                    {keys.data.map(key => (
+                      <TableRow key={key.id}>
+                        <td className="px-6 text-sm text-neutral-200 text-left whitespace-nowrap">{key.name}</td>
+
+                        <td className="px-6 text-sm text-neutral-200 text-left whitespace-nowrap">
+                          <Code>{key.keyStart}</Code>
+                        </td>
+
+                        <td className="px-6 text-sm text-neutral-200 text-left whitespace-nowrap">
+                          {key.permissions.length}
+                        </td>
+
+                        <td className="px-6 text-sm text-neutral-200 text-left whitespace-nowrap">
+                          {!key.lastUsed ? (
+                            'N/A'
+                          ) : (
+                            <Tooltip content={formatDateTime(key.lastUsed)}>{formatTimestamp(key.lastUsed)}</Tooltip>
+                          )}
+                        </td>
+
+                        <td className="px-6 text-sm text-neutral-200 text-left whitespace-nowrap">
+                          <Tooltip content={formatDateTime(key.created)}>{formatTimestamp(key.created)}</Tooltip>
+                        </td>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </table>
+
+                {keys.data.length === 0 ? <NoItems /> : null}
+              </div>
+            </Pagination>
+          </ContentWrapper>
+        </Table>
+      )}
     </Container>
   );
 };
---
+++ frontend/src/pages/dashboard/actions/ApiKeyCreateButton.tsx
@@ -0,0 +1,55 @@
+import { httpErrorToHuman } from '@/api/axios';
+import createApiKey from '@/api/me/api/createApiKey';
+import { Button } from '@/elements/button';
+import { Dialog } from '@/elements/dialog';
+import { Input } from '@/elements/inputs';
+import { useToast } from '@/providers/ToastProvider';
+import { useUserStore } from '@/stores/user';
+import { useEffect, useState } from 'react';
+
+export default () => {
+  const { addToast } = useToast();
+  const { addKey } = useUserStore(state => state.keys);
+
+  const [open, setOpen] = useState(false);
+
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    if (!open) {
+      setName('');
+      return;
+    }
+  }, [open]);
+
+  const submit = () => {
+    createApiKey(name)
+      .then(key => {
+        addToast('API key created.', 'success');
+        setOpen(false);
+        addKey({ ...key.api_key, keyStart: key.key });
+      })
+      .catch(msg => {
+        addToast(httpErrorToHuman(msg), 'error');
+      });
+  };
+
+  return (
+    <>
+      <Dialog title={'Create API Key'} onClose={() => setOpen(false)} open={open}>
+        <Input.Label htmlFor={'name'}>Name</Input.Label>
+        <Input.Text id={'name'} name={'name'} value={name} onChange={e => setName(e.target.value)} />
+
+        <Dialog.Footer>
+          <Button onClick={submit} disabled={!name}>
+            Create
+          </Button>
+          <Button style={Button.Styles.Gray} onClick={() => setOpen(false)}>
+            Close
+          </Button>
+        </Dialog.Footer>
+      </Dialog>
+      <Button onClick={() => setOpen(true)}>Create API Key</Button>
+    </>
+  );
+};
