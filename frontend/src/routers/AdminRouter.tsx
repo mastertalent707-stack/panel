5f8213803319dc6c2899c4b77a3b5a828e666806
mastertalent707-stack <mastertalent707@gmail.com>
2025-07-08 22:20:56 +0200

    Add back button to admin sidebar

--- frontend/src/routers/AdminRouter.tsx
+++ frontend/src/routers/AdminRouter.tsx
@@ -3,7 +3,7 @@ import { Route, Routes } from 'react-router';
 import CollapsedIcon from '@/assets/pterodactyl.svg';
 import NotFound from '@/pages/NotFound';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBuilding, faWrench } from '@fortawesome/free-solid-svg-icons';
+import { faBuilding, faReply, faWrench } from '@fortawesome/free-solid-svg-icons';
 import AdminSettings from '@/pages/admin/settings/AdminSettings';
 import AdminHome from '@/pages/admin/AdminHome';
 
@@ -15,6 +15,12 @@ export default () => {
           <div className="h-fit w-full flex flex-col items-center justify-center mt-1 select-none cursor-pointer">
             <img src={CollapsedIcon} className="my-4 h-20" alt="Pterodactyl Icon" />
           </div>
+          <Sidebar.Section>
+            <Sidebar.Link to="/" end>
+              <FontAwesomeIcon icon={faReply} />
+              <span>Back</span>
+            </Sidebar.Link>
+          </Sidebar.Section>
           <Sidebar.Section>
             <Sidebar.Link to="/admin" end>
               <FontAwesomeIcon icon={faBuilding} />
