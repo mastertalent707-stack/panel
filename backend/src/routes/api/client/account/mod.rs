f3bd07a23b292f845b7b5f07cdd239e7ee152cd8
mastertalent707-stack <mastertalent707@gmail.com>
2025-08-30 18:51:53 +0200

    allow updating username/first/lastname

--- backend/src/routes/api/client/account/mod.rs
+++ backend/src/routes/api/client/account/mod.rs
@@ -13,7 +13,6 @@ mod two_factor;
 
 mod get {
     use crate::{
-        models::user::ApiUser,
         response::{ApiResponse, ApiResponseResult},
         routes::api::client::GetUser,
     };
@@ -22,7 +21,7 @@ mod get {
 
     #[derive(ToSchema, Serialize)]
     struct Response {
-        user: ApiUser,
+        user: crate::models::user::ApiUser,
     }
 
     #[utoipa::path(get, path = "/", responses(
@@ -36,9 +35,95 @@ mod get {
     }
 }
 
+mod patch {
+    use crate::{
+        response::{ApiResponse, ApiResponseResult},
+        routes::{
+            ApiError, GetState,
+            api::client::{GetUser, GetUserActivityLogger},
+        },
+    };
+    use axum::http::StatusCode;
+    use serde::{Deserialize, Serialize};
+    use utoipa::ToSchema;
+    use validator::Validate;
+
+    #[derive(ToSchema, Validate, Deserialize)]
+    pub struct Payload {
+        #[validate(
+            length(min = 3, max = 15),
+            regex(path = "*crate::models::user::USERNAME_REGEX")
+        )]
+        #[schema(min_length = 3, max_length = 15)]
+        #[schema(pattern = "^[a-zA-Z0-9_]+$")]
+        username: Option<String>,
+        #[validate(length(min = 2, max = 255))]
+        #[schema(min_length = 2, max_length = 255)]
+        name_first: Option<String>,
+        #[validate(length(min = 2, max = 255))]
+        #[schema(min_length = 2, max_length = 255)]
+        name_last: Option<String>,
+    }
+
+    #[derive(ToSchema, Serialize)]
+    struct Response {}
+
+    #[utoipa::path(patch, path = "/", responses(
+        (status = OK, body = inline(Response)),
+    ))]
+    pub async fn route(
+        state: GetState,
+        mut user: GetUser,
+        activity_logger: GetUserActivityLogger,
+        axum::Json(data): axum::Json<Payload>,
+    ) -> ApiResponseResult {
+        if let Err(errors) = crate::utils::validate_data(&data) {
+            return ApiResponse::json(ApiError::new_strings_value(errors))
+                .with_status(StatusCode::BAD_REQUEST)
+                .ok();
+        }
+
+        if let Some(username) = data.username {
+            user.username = username;
+        }
+        if let Some(name_first) = data.name_first {
+            user.name_first = name_first;
+        }
+        if let Some(name_last) = data.name_last {
+            user.name_last = name_last;
+        }
+
+        sqlx::query!(
+            "UPDATE users
+            SET username = $2, name_first = $3, name_last = $4
+            WHERE users.uuid = $1",
+            user.uuid,
+            user.username,
+            user.name_first,
+            user.name_last
+        )
+        .execute(state.database.write())
+        .await?;
+
+        activity_logger
+            .log(
+                "user:account.update",
+                serde_json::json!({
+                    "username": user.username,
+                    "name_first": user.name_first,
+                    "name_last": user.name_last,
+                }),
+            )
+            .await;
+
+        ApiResponse::json(Response {}).ok()
+    }
+}
+
 pub fn router(state: &State) -> OpenApiRouter<State> {
     OpenApiRouter::new()
         .routes(routes!(get::route))
+        .routes(routes!(patch::route))
         .nest("/logout", logout::router(state))
         .nest("/email", email::router(state))
         .nest("/password", password::router(state))
