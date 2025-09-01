b0d966632ae845e397f681d222770e72a6233907
mastertalent707-stack <mastertalent707@gmail.com>
2025-09-01 23:11:23 +0200

    hide close button on notification by default

--- frontend/src/elements/Notification.tsx
+++ frontend/src/elements/Notification.tsx
@@ -2,5 +2,5 @@ import { forwardRef } from 'react';
 import { Notification, NotificationProps } from '@mantine/core';
 
 export default forwardRef<HTMLDivElement, NotificationProps>(({ className, ...rest }, ref) => {
-  return <Notification ref={ref} className={className} radius={'md'} {...rest} />;
+  return <Notification ref={ref} className={className} radius={'md'} withCloseButton={false} {...rest} />;
 });
