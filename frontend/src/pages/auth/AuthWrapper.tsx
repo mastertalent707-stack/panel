83e21f7b5e57fe458ddcabdd7d929d81bfe86039
mastertalent707-stack <mastertalent707@gmail.com>
2025-11-10 12:22:36 +0100

    "Responsive" login page

--- frontend/src/pages/auth/AuthWrapper.tsx
+++ frontend/src/pages/auth/AuthWrapper.tsx
@@ -2,12 +2,14 @@ import minecraftBackground from '@/assets/minecraft_background.webp';
 
 export default ({ title, children }: { title?: string; children: React.ReactNode }) => {
   return (
-    <div className={'flex h-screen'}>
-      <div className={'w-1/2 h-full'}>
-        <img src={minecraftBackground} alt={'Minecraft Background'} className={'w-full h-full object-cover'} />
-      </div>
-      <div className={'w-1/2 h-full'}>
-        <div className={'flex flex-col items-center justify-center h-full'}>
+    <div className={'flex items-center justify-center h-screen'}>
+      <img
+        src={minecraftBackground}
+        alt={'Minecraft Background'}
+        className={'hidden md:block w-1/2 h-full object-cover opacity-90'}
+      />
+      <div className={'md:w-1/2 h-full'}>
+        <div className={'flex flex-col items-center justify-center h-full px-2 md:px-0'}>
           {title && <h1 className={'text-3xl font-bold text-white mb-4'}>{title}</h1>}
           {children}
         </div>
