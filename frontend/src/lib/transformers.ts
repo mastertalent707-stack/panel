c1a10e167306e4b46bd49bd800be66355260ff8b
mastertalent707-stack <mastertalent707@gmail.com>
2025-12-12 16:42:47 +0100

    make biome happy

--- frontend/src/lib/transformers.ts
+++ frontend/src/lib/transformers.ts
@@ -6,7 +6,7 @@ export const toSnakeCase = (str: string): string => {
   return str.replace(/([A-Z])/g, '_$1').toLowerCase();
 };
 
-export const transformKeysToCamelCase = (obj: any): object => {
+export const transformKeysToCamelCase = (obj: Record<string, object>): object => {
   if (Array.isArray(obj)) {
     return obj.map((item) => transformKeysToCamelCase(item));
   } else if (obj !== null && typeof obj === 'object') {
@@ -14,7 +14,7 @@ export const transformKeysToCamelCase = (obj: any): object => {
     for (const key in obj) {
       if (Object.hasOwn(obj, key)) {
         const newKey = toCamelCase(key);
-        newObj[newKey] = transformKeysToCamelCase(obj[key]);
+        newObj[newKey] = transformKeysToCamelCase(obj[key] as Record<string, object>);
       }
     }
     return newObj;
@@ -22,7 +22,7 @@ export const transformKeysToCamelCase = (obj: any): object => {
   return obj;
 };
 
-export const transformKeysToSnakeCase = (obj: any): object => {
+export const transformKeysToSnakeCase = (obj: Record<string, object>): object => {
   if (Array.isArray(obj)) {
     return obj.map((item) => transformKeysToSnakeCase(item));
   } else if (obj !== null && typeof obj === 'object') {
@@ -30,7 +30,7 @@ export const transformKeysToSnakeCase = (obj: any): object => {
     for (const key in obj) {
       if (Object.hasOwn(obj, key)) {
         const newKey = toSnakeCase(key);
-        newObj[newKey] = transformKeysToSnakeCase(obj[key]);
+        newObj[newKey] = transformKeysToSnakeCase(obj[key] as Record<string, object>);
       }
     }
     return newObj;
@@ -55,9 +55,12 @@ export const base64ToArrayBuffer = (base64String: string): ArrayBuffer => {
     }
 
     return bytes.buffer;
-  } catch (error: any) {
+  } catch (error) {
     console.error('Error decoding base64 string:', error);
     console.error('Problematic string:', base64String);
-    throw new Error('Failed to decode base64 string: ' + error.message);
+    throw new Error(
+      'Failed to decode base64 string: ' +
+        (error && typeof error === 'object' && 'message' in error && error?.message) || 'Unknown error',
+    );
   }
 };
