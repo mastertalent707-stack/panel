2c66631a59e49a085fe68314073dbc3c74ff68ab
mastertalent707-stack <mastertalent707@gmail.com>
2026-02-25 23:51:02 +0100

    do not kill the admin ui if an invalid version is sent from the api

--- frontend/src/lib/version.ts
+++ frontend/src/lib/version.ts
@@ -6,22 +6,27 @@ class CalagopusVersionInfo {
   public branch?: string;
 
   constructor(version: string) {
-    if (!version.includes('@')) {
-      this.release = new SemVer(version);
-    } else {
-      const [p1, branch] = version.split('@');
+  try {
+			if (!version.includes('@')) {
+				this.release = new SemVer(version);
+			} else {
+				const [p1, branch] = version.split('@');
 
-      this.branch = branch;
+				this.branch = branch;
 
-      if (p1.includes(':')) {
-        const [release, commit] = p1.split(':');
+				if (p1.includes(':')) {
+					const [release, commit] = p1.split(':');
 
-        this.release = new SemVer(release);
-        this.commit = commit;
-      } else {
-        this.release = new SemVer(p1);
-      }
-    }
+					this.release = new SemVer(release);
+					this.commit = commit;
+				} else {
+					this.release = new SemVer(p1);
+				}
+			}
+		} catch (err) {
+			console.error('Error while parsing semver', err);
+			this.release = new SemVer('0.0.0');
+		}
   }
 
   public isStable() {
