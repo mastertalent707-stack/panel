43167f62b167ba6da698ac707a2b854b196a6d46
mastertalent707-stack <mastertalent707@gmail.com>
2026-02-27 21:33:03 +0100

    Fix minor bug

--- frontend/src/pages/server/files/ServerFiles.tsx
+++ frontend/src/pages/server/files/ServerFiles.tsx
@@ -1,13 +1,15 @@
 import { Group, Title } from '@mantine/core';
 import { useQuery } from '@tanstack/react-query';
-import { type Ref, useEffect, useRef } from 'react';
-import { useSearchParams } from 'react-router';
+import { join } from 'pathe';
+import { type Ref, useCallback, useEffect, useRef } from 'react';
+import { createSearchParams, useNavigate, useSearchParams } from 'react-router';
 import loadDirectory from '@/api/server/files/loadDirectory.ts';
 import { ContextMenuProvider } from '@/elements/ContextMenu.tsx';
 import ServerContentContainer from '@/elements/containers/ServerContentContainer.tsx';
 import SelectionArea from '@/elements/SelectionArea.tsx';
 import Spinner from '@/elements/Spinner.tsx';
 import Table from '@/elements/Table.tsx';
+import { isEditableFile, isViewableArchive, isViewableImage } from '@/lib/files.ts';
 import FileActionBar from '@/pages/server/files/FileActionBar.tsx';
 import FileBreadcrumbs from '@/pages/server/files/FileBreadcrumbs.tsx';
 import FileModals from '@/pages/server/files/FileModals.tsx';
@@ -17,23 +19,31 @@ import FileSearchBanner from '@/pages/server/files/FileSearchBanner.tsx';
 import FileSettings from '@/pages/server/files/FileSettings.tsx';
 import FileToolbar from '@/pages/server/files/FileToolbar.tsx';
 import FileUpload from '@/pages/server/files/FileUpload.tsx';
+import { useKeyboardShortcuts } from '@/plugins/useKeyboardShortcuts.ts';
+import { useServerCan } from '@/plugins/usePermissions.ts';
 import { useFileManager } from '@/providers/contexts/fileManagerContext.ts';
 import { FileManagerProvider } from '@/providers/FileManagerProvider.tsx';
+import { useGlobalStore } from '@/stores/global.ts';
 import { useServerStore } from '@/stores/server.ts';
 
 function ServerFilesComponent() {
+  const { settings } = useGlobalStore();
   const { server } = useServerStore();
   const {
     selectedFiles,
     browsingDirectory,
     browsingEntries,
     page,
+    browsingFastDirectory,
     setSelectedFiles,
     setBrowsingEntries,
     setBrowsingWritableDirectory,
     setBrowsingFastDirectory,
+    doOpenModal,
   } = useFileManager();
   const [_, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const canOpenFile = useServerCan('files.read-content');
 
   const { data, isLoading } = useQuery({
     queryKey: ['server', server.uuid, 'files', { browsingDirectory, page }],
@@ -52,7 +62,7 @@ function ServerFilesComponent() {
     if (!data) return;
 
     setBrowsingEntries(data.entries);
-  }
+  };
 
   const previousSelected = useRef<DirectoryEntry[]>([]);
 
@@ -65,6 +75,110 @@ function ServerFilesComponent() {
   };
 
   const onPageSelect = (page: number) => setSearchParams({ directory: browsingDirectory, page: page.toString() });
+
+  const handleOpen = useCallback(
+    (file: DirectoryEntry) => {
+      if (
+        ((isEditableFile(file) || isViewableImage(file)) && file.size <= settings.server.maxFileManagerViewSize) ||
+        file.directory ||
+        (isViewableArchive(file) && browsingFastDirectory)
+      ) {
+        if (file.directory || (isViewableArchive(file) && browsingFastDirectory)) {
+          setSearchParams({
+            directory: join(browsingDirectory, file.name),
+          });
+        } else {
+          if (!canOpenFile) return;
+
+          navigate(
+            `/server/${server.uuidShort}/files/${isViewableImage(file) ? 'image' : 'edit'}?${createSearchParams({
+              directory: browsingDirectory,
+              file: file.name,
+            })}`,
+          );
+        }
+      }
+    },
+    [server.uuidShort, settings, browsingDirectory, browsingFastDirectory, canOpenFile],
+  );
+
+  useKeyboardShortcuts({
+    shortcuts: [
+      {
+        key: 'a',
+        modifiers: ['ctrlOrMeta'],
+        callback: () => setSelectedFiles(new Set(browsingEntries.data)),
+      },
+      {
+        key: 'f',
+        modifiers: ['ctrlOrMeta'],
+        callback: () => doOpenModal('search'),
+      },
+      {
+        key: 'ArrowUp',
+        callback: () => {
+          if (selectedFiles.size === 0) return;
+
+          const selectedIndices = [...selectedFiles.keys()]
+            .map((file) => browsingEntries.data.findIndex((value) => value.name === file.name))
+            .filter((index) => index !== -1);
+
+          if (selectedIndices.length === 0) return;
+
+          const minIndex = Math.min(...selectedIndices);
+          if (minIndex <= 0) return;
+
+          const nextFiles = selectedIndices.map((index) => browsingEntries.data[index - 1]);
+
+          setSelectedFiles(new Set(nextFiles));
+        },
+      },
+      {
+        key: 'ArrowDown',
+        callback: () => {
+          if (selectedFiles.size === 0) return;
+
+          const selectedIndices = [...selectedFiles.keys()]
+            .map((file) => browsingEntries.data.findIndex((value) => value.name === file.name))
+            .filter((index) => index !== -1);
+
+          if (selectedIndices.length === 0) return;
+
+          const maxIndex = Math.max(...selectedIndices);
+          if (maxIndex >= browsingEntries.data.length - 1) return;
+
+          const nextFiles = selectedIndices.map((index) => browsingEntries.data[index + 1]);
+
+          setSelectedFiles(new Set(nextFiles));
+        },
+      },
+      {
+        key: 'ArrowUp',
+        modifiers: ['alt'],
+        callback: () =>
+          setSearchParams({
+            directory: join(browsingDirectory, '..'),
+          }),
+      },
+      {
+        key: 'f2',
+        callback: () => {
+          if (selectedFiles.size === 1) {
+            doOpenModal('rename', [[...selectedFiles.keys()][0]]);
+          }
+        },
+      },
+      {
+        key: 'Enter',
+        callback: () => {
+          if (selectedFiles.size === 1) {
+            handleOpen([...selectedFiles.keys()][0]);
+          }
+        },
+      },
+    ],
+    deps: [browsingEntries.data, selectedFiles, handleOpen],
+  });
 
   return (
     <div className='h-fit relative'>
@@ -109,6 +223,7 @@ function ServerFilesComponent() {
                     <FileRow
                       ref={innerRef as Ref<HTMLTableRowElement>}
                       file={entry}
+                      handleOpen={() => handleOpen(entry)}
                       isSelected={selectedFiles.has(entry)}
                       multipleSelected={selectedFiles.size > 1}
                     />
