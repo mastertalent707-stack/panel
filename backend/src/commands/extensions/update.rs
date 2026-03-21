a2849b043f13de9c7e70e00f888b2d9b5c666608
mastertalent707-stack <mastertalent707@gmail.com>
2026-03-21 18:14:23 +0100

    fix extension tsgo

--- backend/src/commands/extensions/update.rs
+++ backend/src/commands/extensions/update.rs
@@ -99,6 +99,8 @@ impl shared::extensions::commands::CliCommand<UpdateArgs> for UpdateCommand {
                 tokio::fs::create_dir_all(&backend_path).await?;
                 let migrations_path = Path::new("database/extension-migrations")
                     .join(extension_distr.metadata_toml.get_package_identifier());
+                tokio::fs::remove_dir_all(&migrations_path).await?;
+                tokio::fs::create_dir_all(&migrations_path).await?;
 
                 let mut extension_distr = tokio::task::spawn_blocking(move || {
                     extension_distr.extract_frontend(frontend_path)?;
