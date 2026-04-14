cd32865e1986dcb4be175e7d13ce094605a33f6d
mastertalent707-stack <mastertalent707@gmail.com>
2026-04-14 09:14:36 +0200

    fix typescript errors

--- frontend/src/lib/schemas/admin/servers.ts
+++ frontend/src/lib/schemas/admin/servers.ts
@@ -103,7 +103,7 @@ export const adminServerUpdateSchema = z.lazy(() =>
 
 export const adminServerBackupSchema = z.object({
   uuid: z.string(),
-  server: adminServerSchema,
+  server: z.lazy(() => adminServerSchema).nullable(),
   name: z.string(),
   ignoredFiles: z.array(z.string()),
   isSuccessful: z.boolean(),
