4107df1bcd9f6d9253dc21df66121d3f2aeaa4ff
mastertalent707-stack <mastertalent707@gmail.com>
2026-05-19 15:15:00 +0200

    Fix hoverable card colors

--- frontend/src/elements/Card.tsx
+++ frontend/src/elements/Card.tsx
@@ -16,7 +16,7 @@ const Card = forwardRef<HTMLDivElement, CardProps & ComponentProps<'div'>>(
         className={classNames(
           'relative',
           className,
-          hoverable && 'transition-all! duration-190 hover:border-(--mantine-color-default-border)/20! cursor-pointer',
+          hoverable && 'transition-all! duration-190 hover:bg-white/2! hover:light:bg-black/3! cursor-pointer',
         )}
         pl={typeof pl === 'number' && leftStripeClassName ? pl + 4 : leftStripeClassName ? 20 : pl}
         radius='md'
